d8926b21322c41b2cc63c3fa6dcb0a4b89a258ff
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-06 19:20:51 +0530

    displaying time with milli second

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+console.log("JavaScript Loading");
+let HH;
+let MM;
+let SS;
+let MS;
+let AmPm = "AM";
+let d = new Date();
+let b = document.getElementById("button");
+let sc = document.getElementById("screen");
+sc.value = "";
+
+b.addEventListener("click", () => {
+  console.log("Time Strated");
+  setInterval(() => {
+    d = new Date();
+    HH = d.getHours();
+    MM = d.getMinutes();
+    SS = d.getSeconds();
+    MS = d.getMilliseconds();
+    if (HH > 12) {
+      HH -= 12;
+      AmPm = "PM";
+    }
+    sc.value = HH + " : " + MM + " : " + SS + " : " + MS + "   " +AmPm;
+    // console.log("time");
+  }, 1);
+});
