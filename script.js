e6428a440435419eceda4001398b78efa5184481
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-06 20:09:37 +0530

    Date and time optimised

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("JavaScript Loading");
-let HH;
-let MM;
-let SS;
-let MS;
-let AmPm = "AM";
+// let HH;
+// let MM;
+// let SS;
+// let MS;
+// let AmPm = "AM";
 let d = new Date();
 let b = document.getElementById("button");
 let sc = document.getElementById("screen");
@@ -13,15 +13,11 @@ b.addEventListener("click", () => {
   console.log("Time Strated");
   setInterval(() => {
     d = new Date();
-    HH = d.getHours();
-    MM = d.getMinutes();
-    SS = d.getSeconds();
-    MS = d.getMilliseconds();
     if (HH > 12) {
       HH -= 12;
       AmPm = "PM";
     }
-    sc.value = HH + " : " + MM + " : " + SS + " : " + MS + "   " +AmPm;
+    sc.value = d.toLocaleDateString() + "   " + d.toLocaleTimeString();
     // console.log("time");
-  }, 1);
+  }, 75);
 });
